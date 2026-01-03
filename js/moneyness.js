1eb0508e7d00e2806def1c89f7bb56a82228b6f1
Niwea81 <circularevo@gmail.com>
2026-01-03 18:15:51 -0300

    Update moneyness.js

--- js/moneyness.js
+++ js/moneyness.js
@@ -1,1 +1,20 @@
+function getMoneyness(estrategia, perfil) {
+
+  if (estrategia === "Spread") {
+    return {
+      compra: "ATM",
+      venda: "OTM",
+      dte: "30–45 dias"
+    };
+  }
+
+  if (estrategia === "Opcao Comprada") {
+    return {
+      compra: "ATM ou ITM leve",
+      dte: "30–45 dias"
+    };
+  }
+
+  return {};
+}
 
